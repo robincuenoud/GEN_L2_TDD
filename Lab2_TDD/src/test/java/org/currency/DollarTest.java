86e659882721e25d297a036bc4a3408c740221d5
robincuenoud <48149097+robincuenoud@users.noreply.github.com>
2020-03-22 17:17:10 +0100

    Degenerate Objects N.1 , Fixes #4

--- Lab2_TDD/src/test/java/org/currency/DollarTest.java
+++ Lab2_TDD/src/test/java/org/currency/DollarTest.java
@@ -10,9 +10,11 @@ public class DollarTest {
     @Test
     public void testMultiplication()
     {
-        Dollar five=new Dollar(5);
-        five.times(2);
-        assertEquals(10,five.amount);
+        Dollar five= new Dollar(5);
+        Dollar product= five.times(2);
+        assertEquals(10, product.amount);
+        product= five.times(3);
+        assertEquals(15, product.amount);
     }
 
 }
