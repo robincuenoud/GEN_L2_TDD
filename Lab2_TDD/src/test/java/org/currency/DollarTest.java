54b0a9a24938fab0e31a3c4ea70c72e9ec5e1dd9
robincuenoud <48149097+robincuenoud@users.noreply.github.com>
2020-03-22 17:35:29 +0100

    Privacy , fixes #3

--- Lab2_TDD/src/test/java/org/currency/DollarTest.java
+++ Lab2_TDD/src/test/java/org/currency/DollarTest.java
@@ -10,11 +10,9 @@ public class DollarTest {
     @Test
     public void testMultiplication()
     {
-        Dollar five= new Dollar(5);
-        Dollar product= five.times(2);
-        assertEquals(10, product.amount);
-        product= five.times(3);
-        assertEquals(15, product.amount);
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
     @Test
     public void testEquality() {
--- Lab2_TDD/src/test/java/org/example/AppTest.java
+++ Lab2_TDD/src/test/java/org/example/AppTest.java
@@ -12,8 +12,6 @@ public class AppTest
 {
     public void testMultiplication()
     {
-        Dollar five=new Dollar(5);
-        five.times(2);
-        assertEquals(10,five.amount);
+
     }
 }
